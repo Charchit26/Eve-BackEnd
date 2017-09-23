dd29662e2fdbf8fce457e04c6f7533ad54f9247e
Charchit <charchit26@gmail.com>
2017-09-23 16:45:05 +0530

    hi working

--- src/main/java/bitoflife/chatterbean/AliceBotMother.java
+++ src/main/java/bitoflife/chatterbean/AliceBotMother.java
@@ -16,10 +16,20 @@ package bitoflife.chatterbean;
 
 import java.io.FileInputStream;
 import java.io.InputStream;
+
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.PrintStream;
 import java.net.URL;
+import java.net.URLClassLoader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 
 import bitoflife.chatterbean.parser.AliceBotParser;
 import bitoflife.chatterbean.util.Searcher;
@@ -29,7 +39,7 @@ public class AliceBotMother
   /*
   Attribute Section
   */
-  
+
   private ByteArrayOutputStream gossip;
   
   /*
@@ -57,7 +67,7 @@ public class AliceBotMother
 /*    InputStream is = this.getClass().getClassLoader().getResourceAsStream("Bots/context.xml");
     System.out.println(is.read());
 */
-/*    File folder = new File(url.toURI());
+    /*File folder = new File(url.toURI());
     File[] listOfFiles = folder.listFiles();
 
         for (int i = 0; i < listOfFiles.length; i++) {
@@ -67,11 +77,37 @@ public class AliceBotMother
             System.out.println("Directory " + listOfFiles[i].getName());
           }
         }*/
-    AliceBot bot = parser.parse(new FileInputStream("classes/Bots/context.xml"),
-                                new FileInputStream("classes/Bots/splitters.xml"),
-                                new FileInputStream("classes/Bots/substitutions.xml"),
-                                searcher.search("classes/Bots/Alice", ".*\\.aiml"));
-
+    Resource resource1 = new ClassPathResource("Bots/context.xml");
+    InputStream inputStream1 = resource1.getInputStream();
+    Path ctx = Files.createTempFile("context", ".xml");
+    Files.copy(inputStream1, ctx, StandardCopyOption.REPLACE_EXISTING);
+    
+    Resource resource2 = new ClassPathResource("Bots/splitters.xml");
+    InputStream inputStream2 = resource2.getInputStream();
+    Path splitters = Files.createTempFile("splitters", ".xml");
+    Files.copy(inputStream2, splitters, StandardCopyOption.REPLACE_EXISTING);
+    
+    Resource resource3 = new ClassPathResource("Bots/substitutions.xml");
+    InputStream inputStream3 = resource3.getInputStream();
+    Path substitutions = Files.createTempFile("substitutions", ".xml");
+    Files.copy(inputStream3, substitutions, StandardCopyOption.REPLACE_EXISTING);
+    
+    Resource resource5 = new ClassPathResource("Bots/mainFile.aiml");
+    InputStream inputStream5 = resource5.getInputStream();
+    Path mainFile = Files.createTempFile("mainFile", ".aiml");
+    Files.copy(inputStream5, mainFile, StandardCopyOption.REPLACE_EXISTING);
+    
+/*    Resource resource4 = new ClassPathResource("Bots/Alice");
+    InputStream inputStream4 = resource4.getInputStream();
+    Path alice = Files.createTempDirectory("alice");
+    Files.copy(inputStream4, alice, StandardCopyOption.REPLACE_EXISTING);*/
+System.out.println("files mil gyi");
+    AliceBot bot = parser.parse(new FileInputStream(ctx.toFile()),
+    							new FileInputStream(splitters.toFile()),
+    							new FileInputStream(substitutions.toFile()),
+    							new FileInputStream(mainFile.toFile()));
+    							//searcher.search("Bots/Alice", ".*\\.aiml"));
+System.out.println("fdsf");
     Context context = bot.getContext(); 
     context.outputStream(gossip);
     return bot;
